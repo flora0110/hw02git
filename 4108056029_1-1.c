fae4997e42b0a1f54576ac0e167e92057ef0bbe5
flora <flora90.1.10@gmail.com>
2020-11-01 15:42:31 +0800

    mark 有問題

--- 4108056029_1-1.c
+++ 4108056029_1-1.c
@@ -24,7 +24,7 @@ node* queueEmpty(){
     return item;
 }
 void addq(node* item){
-    printf("in addq\n");
+    //printf("in addq\n");
     queuepointer temp;
     temp=(queuepointer)malloc(sizeof(queue));
     temp->nodelink=item;
@@ -53,9 +53,10 @@ node* deleteq(){
 }
 
 typedef struct Nodenum{
-    int num;
+    double num;
     node* fromnode;
     int no;
+    int p;
 }Nodenum;
 
 
@@ -113,13 +114,13 @@ Nodenum minno(int i){
     }
     return temp;
 }
-node* maxno(int i){
+Nodenum maxno(int i){
     int max=-1;
-    node* temp;
+    Nodenum temp;
     queuepointer2 now=front2[i];
     while(now){
         if(now->data.num>max){
-            temp=now->data.fromnode;
+            temp=now->data;
             now=now->link;
         }
     }
@@ -131,7 +132,6 @@ void postorder(node* ptr){
         printf("%d\n",ptr->data);
         postorder(ptr->left);
         postorder(ptr->right);
-
     }
 }
 int main(){
@@ -139,9 +139,11 @@ int main(){
     char c;
     //node* Root;
     node* root;
+    root=(node*)malloc(sizeof(node));
     c=getc(stdin);
     int n=0;
     //printf("[: %c\n",c);
+    int test=1;
     if(c=='['){//開始建立樹
         //printf("[: %c\n",c);
         c=getc(stdin);//抓根
@@ -150,14 +152,14 @@ int main(){
             c=getc(stdin);
             //struct TreeNode root;
             printf("??\n");
-            root->data=1;
-            printf("ready to addq\n");
+            root->data=test++;
+            //printf("ready to addq\n");
             addq(root);
-            printf("out addq\n");
+            //printf("out addq\n");
             n++;
         }
         while(front){
-            printf("in while\n" );
+            //printf("in while\n" );
             struct TreeNode* current;//現在在幫誰加葉子
             current=deleteq();
             if(c==']'){//結束
@@ -171,18 +173,22 @@ int main(){
                 newnode=(node*)malloc(sizeof(node));
                 //current.left=&newnode;
                 current->left=newnode;
-
-                newnode->data=1;
-                current->data=2;
+                newnode->data=test++;
+                newnode->left=NULL;
+                newnode->right=NULL;
                 //printf("root's data %d\n",current.data);
                 addq(newnode);
+                printf("out addq\n");
                 n++;
             }
             else if(c=='n'){
                 current->left=NULL;
                 scanf("%*[ull]");//吃掉ull
             }
-            getc(stdin);//吃','
+            c=getc(stdin);//吃','
+            if(c==']'){
+                break;
+            }
             c=getc(stdin);
             if(c=='0'){
                 printf("right: %c\n",c);
@@ -190,8 +196,9 @@ int main(){
                 node* newnode;
                 newnode=(node*)malloc(sizeof(node));
                 current->right=newnode;
-
-                newnode->data=1;
+                newnode->left=NULL;
+                newnode->right=NULL;
+                newnode->data=test++;
                 addq(newnode);
                 n++;
             }
@@ -211,16 +218,19 @@ int main(){
     printf("start to find\n");
     int lleaf,rleaf;
     //Nodenum nodenum[n][n];
-    int den=0;//現在在檢查的有幾個子
-    int leafden=0;//現在在檢查的的葉子有幾個子
+    double den=0;//現在在檢查的有幾個子
+    double leafden=0;//現在在檢查的的葉子有幾個子
     front=NULL;
     addq(root);
     int nownum=-1;//在檢查誰
     int leafnum=0;//丟值給子節點到幾號了
+    int k=0;
     //------------------------------------
-    while(front!=rear){
+    while(front){
         struct TreeNode* nowcheck=deleteq();
         nownum++;
+        printf("now check %d\n",nownum );
+        den=0;
         if(nowcheck->left){
             addq(nowcheck->left);//加到之後要檢查的
             den++;
@@ -229,10 +239,13 @@ int main(){
             addq(nowcheck->right);
             den++;
         }
+
         Nodenum nodenum;
-        nodenum.num=nowcheck->data/(den+1);;
+        nodenum.num=(double)nowcheck->data/(den+1+k);
         nodenum.fromnode=nowcheck;
-        nodenum.no=0;
+        nodenum.no=nownum;//從幾號來
+        printf("nowcheck->data : %d\n",nowcheck->data);
+        printf("nowcheck->data/(den+1+%d) : %f to %d\n",k,nodenum.num,nownum);
         //nodenum.no=nownum;
         addq2(nodenum,nownum);//把自己的值/子+1 來自自己 addq2 queue2[nownum] 這個是給現在檢查的位置
         //nodenum[nownum][nowno].num=nowcheck.data/(den+1);//自己的值/den+1
@@ -240,9 +253,10 @@ int main(){
         if(nowcheck->left){
             leafnum++;//在給幾號葉子值
             Nodenum leftnum;//父要給左子
-            leftnum.num=nowcheck->data/(den+1);//父節點給子的值
+            leftnum.num=nowcheck->data/(den+1+k);//父節點給子的值
             leftnum.fromnode=nowcheck;//來自父nownum
-            leftnum.no=0;
+            leftnum.no=nownum;
+            printf("nowcheck->data/(den+1) : %f to %d\n",leftnum.num,leafnum);
             //leftnum.no=nodenum.no+1;//給左子下一個編號
             addq2(leftnum,leafnum);
             //nodenum[1][0].num=nowcheck.data/(den+1);
@@ -255,17 +269,19 @@ int main(){
                 leafden++;
             }
             //左子要給父
-            leftnum.no=0;
-            leftnum.num=nowcheck->left->data/(leafden+2);
+            leftnum.no=leafnum;
+            leftnum.num=(double)nowcheck->left->data/(leafden+2);
             leftnum.fromnode=nowcheck->left;//來自佐子
+            printf("leftnum.num : %f to %d\n",leftnum.num,nownum);
             addq2(leftnum,nownum);
         }
         if(nowcheck->right){
             leafnum++;//在給幾號葉子值
             Nodenum rightnum;//父要給右子
-            rightnum.num=nowcheck->data/(den+1);//父節點給子的值
+            rightnum.num=nowcheck->data/(den+1+k);//父節點給子的值
             rightnum.fromnode=nowcheck;//來自父nownum
-            rightnum.no=0;
+            rightnum.no=nownum;
+            printf("nowcheck->data/(den+1) : %f to %d\n",rightnum.num,leafnum);
             addq2(rightnum,leafnum);
 
             //開始檢查左子有幾個子
@@ -277,26 +293,38 @@ int main(){
                 leafden++;
             }
             //右子要給父
-            rightnum.no=0;
-            rightnum.num=nowcheck->right->data/(leafden+2);
+            rightnum.no=leafnum;
+            rightnum.num=(double)nowcheck->right->data/(leafden+2);
             rightnum.fromnode=nowcheck->right;//來自右子
+            printf("rightnum.num : %f to %d\n",rightnum.num,nownum);
             addq2(rightnum,nownum);
         }
+        k=1;
     }
     printf("finish find\n");
     printf("start to mark\n");
     int min=15001;
+    int mark[nownum];
+    int pro[nownum];
+    for(i=0;i<nownum;i++){
+        mark[i]=0;
+        pro[i]=0;
+    }
     for(i=0;i<nownum;i++){
         Nodenum minnodenum=minno(i);
-        minnodenum.no++;
+        printf("min to protect %d: %d\n", i,minnodenum.no);
+        mark[minnodenum.no]++;
     }
-    int mark[nownum];
+
     int ans=0;
-    for(i=0;i<nownum;i++){
-        node marknode=*maxno(i);
-        if(marknode.data!=-1){
-            ans++;
-            marknode.data=-1;
+    for(i=nownum-1;i>=0;i--){
+        if(pro[i]==0){//沒被保護
+            Nodenum marknode=maxno(i);//-1 插旗 找一個最小代價又可同時保護最多人的店去保護他
+            if(marknode.fromnode->data!=-1){//還沒被插旗
+                ans++;
+                marknode.fromnode->data=-1;
+            }
+            pro[i]=1;
         }
     }
 }
