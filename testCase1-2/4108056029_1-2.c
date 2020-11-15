7c95a86eaf1b73efb7e2258e288d075a985e676a
flora <flora90.1.10@gmail.com>
2020-11-16 05:05:41 +0800

    新方法 架構玩

--- testCase1-2/4108056029_1-2.c
+++ testCase1-2/4108056029_1-2.c
@@ -103,30 +103,43 @@ element deleteq2(){
     free(temp);
     return item;
 }
-typedef struct List* listpointer;
-typedef struct List{
-    int* data;
-    listpointer link;
-}list;
-listpointer front3=NULL,rear3;
-void addq3(int* item){
-    listpointer temp=(listpointer)malloc(sizeof(list));
-    temp->data=item;
-    temp->link=NULL;
-    if(!front3){
-        front3=temp;
-    }
-    else{
-        rear3->link=temp;
-    }
-    rear3=temp;
-}
-int* deleteq3(){
-    listpointer temp=front3;
-    int* item;
+typedef struct stack* stackpointer;
+typedef struct stack{
+    node* nodeptr;
+    stackpointer link;
+    stackpointer qulink;
+}stack;
+stackpointer top=NULL,front3=NULL,rear3;
+int nodeptrn=0;
+void push(node* item){
+    stackpointer temp=(stackpointer)malloc(sizeof(stack));
+    temp->nodeptr=item;
+    temp->link=top;
+    temp->qulink=NULL;
     if(front3){
-        item=front3->data;
-        front3=temp->link;
+        rear3->qulink=temp;
+    }
+    else
+        front3=temp;
+    rear3=temp;
+    top=temp;
+    nodeptrn++;
+    if(nodeptrn>5){
+        stackpointer temp2=front3;
+        if(!temp2){
+            printf("error\n");
+        }
+        front3=temp2->qulink;
+        free(temp2);
+        nodeptrn--;
+    }
+}
+node* pop(){
+    stackpointer temp=top;
+    node* item;
+    if(top){
+        item=top->nodeptr;
+        top=temp->link;
         free(temp);
         return item;
     }
@@ -135,6 +148,7 @@ int* deleteq3(){
 typedef struct imfor{
     int data;//節點裡的值
     int mark;//標記
+    int temp[3];//c1 c2 f
 }imfor;
 int min;
 int numarray[15000];
@@ -143,8 +157,47 @@ int sum=0;
 int count=0;
 int mincount;
 int checkn=0;//需要check 的點
-queuepointer2 top;
-imfor find (node* ptr,int fatherdata,int firsttime){
+stackpointer top;
+imfor find (node* ,node* ,char ,int );
+int nearby(char c,int sum){
+    node* a=pop();
+    if(c=='l'){
+        imfor ar=find(a->right,a,'r',1);
+        if(ar.mark==-2 || ar.mark==-4){
+            push(a);
+            return 1;
+        }
+        if(ar.mark==-3){
+            if(ar.temp[1]==-1){
+                ar.temp[1]=0;
+            }
+            if((ar.data-(ar.temp[0]+ar.temp[1])) < sum ){
+                push(a);
+                return 1;
+            }
+            else {
+                ar.mark=-1;
+            }
+
+        }
+        if(ar.mark==-1){
+            node *f=pop();
+            if(f==NULL){
+                return 0;
+            }
+            else{
+                f->data-=sum;
+                push(f);
+                push(a);
+                return 3;
+            }
+        }
+
+    }
+}
+imfor find (node* ptr,node* fatherptr,char c,int switch2){
+    push(ptr);
+    int i;
     //printf("in find\n");
     imfor nowimfor;
     if(ptr==NULL){
@@ -156,12 +209,68 @@ imfor find (node* ptr,int fatherdata,int firsttime){
     //printf("------------ptr->data %d\n",ptr->data);
     nowimfor.data=ptr->data;
     nowimfor.mark=0;
-    imfor leftimfor=find(ptr->left,ptr->data,firsttime);
-    imfor rightimfor=find(ptr->right,ptr->data,firsttime);
+    int fatherdata=fatherptr->data;
+    imfor leftimfor=find(ptr->left,ptr,'l',switch2);
+    imfor rightimfor=find(ptr->right,ptr,firsttime,'r',switch2);
     int rightdata=rightimfor.data;
     int rightmark=rightimfor.mark;
     int leftdata=leftimfor.data;
     int leftmark=leftimfor.mark;
+    int temp1[3];
+    int temp2[3];
+    int leaf1[3];
+    int leaf2[3];
+    int isf1=0,isf2=0;
+    int leaff1=0,leaff2=0;
+    if(leftmark==-9){
+        for(i=0;i<3;i++){
+            nowimfor.temp[i]=leftimfor.temp[i];
+        }
+        nowimfor.mark=-10;
+        return nowimfor;
+    }
+    if(rightmark==-9){
+        for(i=0;i<3;i++){
+            nowimfor.temp[i]=rightimfor.temp[i];
+        }
+        nowimfor.mark=-10;
+        return nowimfor;
+    }
+    if(leftmark==-10&&rightmark==-1 || rightmark==-10&&leftmark==-1){
+        for(i=0;i<3;i++){
+            nowimfor.temp[i]=leftimfor.temp[i];
+        }
+        nowimfor.mark=-20;
+        return nowimfor;
+    }
+    if(leftmark==-20){
+        for(i=0;i<3;i++){
+            leaf1[i]=leftimfor.temp[i];
+        }
+        leftmark=-2;
+        leaff1=1;
+    }
+    if(rightmark==-20){
+        for(i=0;i<3;i++){
+            leaf2[i]=rightimfor.temp[i];
+        }
+        rightmark=-2;
+        leaff2=1;
+    }
+    if(leftmark==-10){
+        for(i=0;i<3;i++){
+            temp1[i]=leftimfor.temp[i];
+        }
+        leftmark=-1;
+        isf1=1;
+    }
+    if(rightmark==-10){
+        for(i=0;i<3;i++){
+            temp2[i]=rightimfor.temp[i];
+        }
+        rightmark=-1;
+        isf2=1;
+    }
     //printf("ptr->data %d leftmark: %d    rightmark: %d\n",ptr->data,leftmark,rightmark );
     if(leftmark==-4){
         nowimfor.mark=-6;
@@ -172,32 +281,128 @@ imfor find (node* ptr,int fatherdata,int firsttime){
         nowimfor.mark=-6;
         rightmark=-1;
         rightdata=0;
-    }
-    if(leftmark==-5 || leftmark==-6){
-        leftmark=-1;
-        leftdata=0;
-    }
-    if(rightmark==-5 || rightmark==-6){
-        rightmark=-1;
-        rightdata=0;
+        if(leftmark==-1){
+            nowimfor.mark=-1;
+        }
     }
     //printf("\nnowmark %d\n",nowimfor.mark );
     //printf("leftmark: %d    rightmark: %d\n",leftmark,rightmark );
-    if(leftmark==-1 && rightmark==-1 && nowimfor.mark!=-6){
+    if(leftmark==-1 && rightmark==-1 && nowimfor.mark!=-1){
         //printf("-------------------%d is x\n",ptr->data);
         nowimfor.mark=-2;
     }
     else if(leftmark==-2 || rightmark==-2){
-        if(ptr->data<(rightdata+leftdata)){
+        //nowimfor.mark=-3;
+        int case=0;
+        if(ptr->data>(rightdata+leftdata) && ptr->data<(rightdata+leftdata+fatherdata)){
+            nowimfor.mark=-3;
+            if(swich2){
+                int p;
+                if(leftmark==-2){
+                    nowimfor.temp[p++]=leftdata;
+                }
+                if(rightmark==-2){
+                    nowimfor.temp[p++]=rightdata;
+                }
+                nowimfor.temp[2]=ptr->data;
+                return nowimfor;
+            }
+            int check = nearby(c,ptr->data-leftdata-rightdata);
+            if(check==1){
+                case=-5;
+            }
+            else if(case==0){
+                case=-4;
+            }
+            else if(check==3){
+                nowimfor.mark=-9;
+            }
+        }
+        if(ptr->data<(rightdata+leftdata) ||(case==-4)){
             //printf("------------------------%d is case -4\n",ptr->data);
+            //先恢復ptr->data
+            if(isf1){
+                if(temp1[1]==-1){
+                    ptr->data+=temp1[2]-temp1[0];
+                }
+                else{
+                    ptr->data+=temp1[2]-temp1[1]-temp1[0];
+                }
+            }
+            if(isf2){
+                if(temp2[1]==-1){
+                    ptr->data+=temp2[2]-temp2[0];
+                }
+                else{
+                    ptr->data+=temp2[2]-temp2[1]-temp2[0];
+                }
+            }
+            //----------------------
+            if(isf1){
+                if(temp1[1]!=-1){
+                    numarray[count++]=temp1[1];
+                    sum+=temp1[0]+temp1[1];
+                }
+                else{
+                    sum+=temp1[0];
+                }
+                numarray[count++]=temp1[0];
+            }
+            if(isf2){
+                if(temp2[1]!=-1){
+                    numarray[count++]=temp2[1];
+                    sum+=temp2[0]+temp2[1];
+                }
+                else{
+                    sum+=temp2[0];
+                }
+                numarray[count++]=temp2[0];
+            }
             nowimfor.mark=-4;
             numarray[count++]=ptr->data;
             sum+=ptr->data;
+            if(leaff1){
+                numarray[count++]=leaf1[2];
+            }
+            if(leaff2){
+                numarray[count++]=leaf2[2];
+            }
             //printf("numarray[%d] %d\n",count-1, numarray[count-1]);
         }
-        else if(ptr->data>(rightdata+leftdata+fatherdata)){
+        else if((ptr->data>(rightdata+leftdata+fatherdata))||case==-5){
+            if(leaff1){
+                if(leaf1[1]==-1){
+                    leftdata+=leaf1[2]-leaf1[0];
+                    ptr->left->data=leftdata;
+                }
+                else{
+                    leftdata+=leaf1[2]-leaf1[1]-leaf1[0];
+                    ptr->left->data=leftdata;
+                }
+            }
+            if(leaff2){
+                if(leaf2[1]==-1){
+                    leftdata+=leaf2[2]-leaf2[0];
+                    ptr->left->data=leftdata;
+                }
+                else{
+                    leftdata+=leaf2[2]-leaf2[1]-leaf2[0];
+                    ptr->left->data=leftdata;
+                }
+            }
             //printf("------------------------%d is case -5\n",ptr->data);
             if(leftmark==-2){
+
+                if(leaff1){
+                    if(leaf1[1]!=-1){
+                        numarray[count++]=leaf1[1];
+                        sum+=leaf1[0]+leaf1[1];
+                    }
+                    else{
+                        sum+=leaf1[0];
+                    }
+                    numarray[count++]=leaf1[0];
+                }
                 sum+=leftdata;
                 //printf("count %d\n",count );
                 numarray[count++]=leftdata;
@@ -208,60 +413,31 @@ imfor find (node* ptr,int fatherdata,int firsttime){
                 sum+=rightdata;
                 //printf("count %d\n",count);
                 numarray[count++]=rightdata;
+                if(leaff2){
+                    if(leaf2[1]!=-1){
+                        numarray[count++]=leaf2[1];
+                        sum+=leaf2[0]+leaf2[1];
+                    }
+                    else{
+                        sum+=leaf2[0];
+                    }
+                    numarray[count++]=leaf2[0];
+                }
                 //printf("right %d\n",rightdata);
                 //printf("numarray[%d] %d\n",count-1, numarray[count-1]);
             }
+            if(isf1){
+                numarray[count++]=temp1[0];
+                numarray[count++]=temp1[1];
+            }
+            if(isf1){
+                numarray[count++]=temp2[0];
+                numarray[count++]=temp2[1];
+            }
             //printf("sum: %d\n",sum );
-            nowimfor.mark=-5;
+            nowimfor.mark=-1;
         }
-        else{
-            //printf("else\n");
-            int way=1;
-            if(firsttime){
-                element e;
-                e.nodepointer=ptr;
-                e.way=1;
-                addq2(e);
-                checkn++;
-            }
-            else{
-                //queuepointer2 qp=front2;
-                //while (qp) {
-                    if(top->e.nodepointer==ptr){
-                        way=top->e.way;
-                        top=top->link;
-                        //break;
-                    }
-                    //qp=qp->link;
-                //}
-            }
-            if(way==1){
-                //printf("\nway 1\n");
-                //printf("-------------------%d is case -4\n",ptr->data);
-                //printf("count %d\n",count);
-                nowimfor.mark=-4;
-                numarray[count++]=ptr->data;
-                sum+=ptr->data;
-                //printf("numarray[%d] %d\n",count-1, numarray[count-1]);
-            }
-            else if(way==2){
-                //printf("\nway 2\n");
-                //printf("-------------------%d is case -5\n",ptr->data);
-                if(leftmark==-2){
-                    //printf("inright\n");
-                    sum+=leftdata;
-                    numarray[count++]=leftdata;
-                    //printf("numarray[%d] %d\n",count-1, numarray[count-1]);
-                }
-                if(rightmark==-2){
-                    //printf("inleft\n");
-                    sum+=rightdata;
-                    numarray[count++]=rightdata;
-                    //printf("numarray[%d] %d\n",count-1, numarray[count-1]);
-                }
-                nowimfor.mark=-5;
-            }
-        }
+
     }
     //printf("finish------------ptr->data %d %d\n",ptr->data,nowimfor.mark);
     return nowimfor;
@@ -269,55 +445,6 @@ imfor find (node* ptr,int fatherdata,int firsttime){
 //--------------------------可能性
 int testn=0;
 node *root;
-void dfs(int *temp,int i){
-    int j;
-    if(i==checkn){
-        //int *item;
-        //int *item=(int*)malloc(checkn*sizeof(int));//小心指標會一起改
-        //MALLOC(item,checkn*sizeof(int));
-        /*for(j=0;j<checkn;j++){
-            item[j]=temp[j];
-        }*/
-        //addq3(item);
-        testn++;
-        printf("%d\n",testn );
-        //-----------------------------------
-        count=0;
-        sum=0;
-        queuepointer2 nptr=front2;
-        for(i=0;i<checkn;i++){
-            nptr->e.way=temp[i];
-            nptr=nptr->link;
-        }
-        top=front2;
-        imfor rootim = find(root,0,0);
-        //printf("rootim %d\n",rootim.mark );
-        if(rootim.mark==-2 ){
-            printf("\n");
-            numarray[count++]=root->data;
-            sum+=root->data;
-        }
-        /*printf("-----------numarray----------######################\n");
-        for(i=0;i<count;i++){
-            printf("%d \n",numarray[i]);
-        }
-        printf("---------------------------\n");*/
-        if(sum<min){
-            for(i=0;i<count;i++){
-                minnum[i]=numarray[i];
-            }
-            min=sum;
-            mincount=count;
-        }
-        //----------------------------------
-        return;
-    }
-    temp[i]=1;
-    dfs(temp,i+1);
-    temp[i]=2;
-    dfs(temp,i+1);
-}
-
 void postorder(node* ptr){
     if(ptr){
         printf("%d\n",ptr->data);
@@ -419,7 +546,7 @@ int main(){
             printf("----------test-----------\n");
             //postorder(root);
             //printf("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
-            imfor rootim =find(root,0,1);//第一次
+            imfor rootim =find(root,NULL,'l',0);//第一次
             printf("checkn %d\n",checkn );//需要檢查的點
             if(rootim.mark==-2 ){
                 numarray[count++]=root->data;
@@ -439,53 +566,8 @@ int main(){
             printf("---------------------------\n");
 
             int *temp=(int*)malloc(count*sizeof(int));
-            if(front2){//queue2存要處理的點
-                dfs(temp,0);//把可能性弄完
-            }
             printf("$%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%testn %d checkn %d\n",testn,checkn );
-            //dfs(temp,0);//把可能性弄完
-            //int *check;//
-            /*
-            while(front3){
-                //printf("in while\n");
-                count=0;
-                check=deleteq3();
-                sum=0;
-                queuepointer2 nptr=front2;
-                for(i=0;i<checkn;i++){
-                    nptr->e.way=check[i];
-                    nptr=nptr->link;
-                }
-                top=front2;
-                rootim = find(root,0,0);
-                //printf("rootim.mark %d\n",rootim.mark);
-                //printf("root data %d\n",root->data );
-                if(rootim.mark==-2 ){
-                    numarray[count++]=root->data;
-                    sum+=root->data;
-                    //printf("numarray[%d] %d\n",count-1, numarray[count-1]);
-                }
-                //printf("-----------numarray----------######################\n");
-                //for(i=0;i<count;i++){
-                //    printf("%d \n",numarray[i]);
-                //}
-                //printf("---------------------------\n");
-                if(sum<min){
-                    //memcpy(minnum,numarray,sizeof(num));
-                    for(i=0;i<count;i++){
-                        minnum[i]=numarray[i];
-                        //printf("%d \n",minnum[i]);
-                    }
-                    min=sum;
-                    mincount=count;
-                    //printf("-----------minnum----------######################\n");
-                    //for(i=0;i<mincount;i++){
-                    //    printf("%d \n",minnum[i]);
-                    //}
-                    //printf("---------------------------\n");
-                }
-            }
-            */
+
         }
         printf("output: %d\n",min);
         for(i=0;i<mincount;i++){
